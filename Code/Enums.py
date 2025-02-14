b15e7427aedeae03cec400f2f72158675f6e0f9b
Jagatpran Amartya <amartyaj@gmail.com>
2025-02-14 14:04:03 -0600

    Added Enums.py

---
+++ Code/Enums.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from enum import Enum
+
+class Season(Enum):
+    SPRING = 1
+    SUMMER = 2
+    AUTUMN = 3
+    WINTER = 4
+
+print(Season.SPRING)
+print(Season.SPRING.name)
+print(Season.SPRING.value)
+print(type(Season.SPRING))
+print(repr(Season.SPRING))
+print(list(Season))
