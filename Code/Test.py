50eedf8fbad4ad7203e19f6603a5e8c8cfce18bb
Jagatpran Amartya <amartyaj@gmail.com>
2025-02-22 10:07:12 -0600

    Decision Tree Basics

---
+++ Code/Test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+def classify(word):
+    result = ""
+
+    if word == "Word 1":
+        result = "Good word"
+    elif word == "Word 2":
+        result = "Bad word"
+    else:
+        result = "Neutral word"
+
+    return result
+
+
+if __name__ == "__main__":
+
+    my_words = ["Word 1", "Word 2", "Word 3"]
+
+    for my_word in my_words:
+        my_result = classify(my_word)
+        print(my_result)
