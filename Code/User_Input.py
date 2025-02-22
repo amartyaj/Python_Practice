7e58007cd64c4ad71ebd2237c20c5b6292e391d7
Jagatpran Amartya <amartyaj@gmail.com>
2025-02-21 20:06:02 -0600

    Mandatory user name input

--- Code/User_Input.py
+++ Code/User_Input.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 
+import re
+
+def clean_up(string):
+    pattern = "^[a-zA-Z]+"
+    result = re.search(pattern, string)
+    return result.group(0)
+
 if __name__ == "__main__":
-    name = input("Please enter your first name: ")
+    while True:
+        name = input("Please enter your first name: ")
+        if name:
+            break
+        else:
+            print("Input cannot be empty, please try again ..")
+
+    name = clean_up(name)
     print(name)
