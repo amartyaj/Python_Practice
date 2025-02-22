768a8ee9ddf3d1304bbbe34c45d6109a292669b9
Jagatpran Amartya <amartyaj@gmail.com>
2025-02-21 19:01:34 -0600

    Fibonacci series: Completed

--- Code/Fibonacci.py
+++ Code/Fibonacci.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
 def calc_fib(n):
-    result = n
-    return result
+    if n < 0:
+        result = None
+        return result
+    elif n == 0:
+        result = n
+        return result
+    elif n == 1:
+        result = 1
+        return result
+    else:
+        result = calc_fib(n - 2) + calc_fib(n - 1)
+        return result
 
 if __name__ == "__main__":
     # print("Test")
-    for i in range(0, 11):
+    for i in range(0, 10):
         fib = calc_fib(i)
-        print(fib)
-
+        print(f"{i + 1}th number in Fibonacci series is: {fib}")
